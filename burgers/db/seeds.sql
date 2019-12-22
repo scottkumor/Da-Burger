245c83b6702220017cfbac0329e27bcb306f2aea
Scott Kumor <scott.kumor1212@gmail.com>
2019-12-22 10:36:30 -0600

    working db manip

---
+++ burgers/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('burger1');
+INSERT INTO burgers (name) VALUES ('burger2');
+INSERT INTO burgers (name, devoured) VALUES ('burger3', true);
+INSERT INTO burgers (name, devoured) VALUES ('burger4', true);
+INSERT INTO burgers (name, devoured) VALUES ('burger5', true);
+INSERT INTO burgers (name) VALUES ('burger6');
